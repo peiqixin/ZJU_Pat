b24016535e8a0d4541693b36b8d5c187f7cf6ef0
qixin pei <peiqixin1995@gmail.com>
2017-08-21 07:56:51 +0800

    1008 AC

--- PATAdvancedLevelPractise/1008.c
+++ PATAdvancedLevelPractise/1008.c
@@ -1,9 +1,21 @@
-
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-	char c;
-	scanf("%c", &c);
-	printf("%c\n", c);
+	int length;
+	scanf("%d" ,&length);
+	int elevator[length];
+	int i = 0;
+	for(i = 0; i < length; i++)
+		scanf("%d", &elevator[i]);
+	int sum = length * 5;
+	int pre = 0;
+	for(i = 0; i < length; i++){
+		if(elevator[i] - pre > 0)
+			sum += (elevator[i] - pre) * 6;
+		else
+			sum += (pre - elevator[i]) * 4;
+		pre = elevator[i];
+	}
+	printf("%d\n", sum);
 	return 0;
 }
