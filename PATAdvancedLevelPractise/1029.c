80f2c7f9307034220f46392c3c7cb07b42d95f53
qixin pei <peiqixin1995@gmail.com>
2017-09-10 16:48:23 +0800

    1029 AC

--- PATAdvancedLevelPractise/1029.c
+++ PATAdvancedLevelPractise/1029.c
@@ -1,42 +1,38 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-	int N1, N2, i, j;
-	scanf("%d", &N1);
-	long int num1[N1];
-	for(i = 0; i < N1; i++)
-		scanf("%ld", &num1[i]);
-
-	scanf("%d", &N2);
-	long int num2[N2];
-	for(i = 0; i < N2; i++)
-		scanf("%ld", &num2[i]);
+	int N, M, i, j;
+	scanf("%d", &N);
+	int S1[N];
+	for(i = 0; i < N; i++)
+		scanf("%d", &S1[i]);
+	scanf("%d", &M);
+	int S2[M];
+	for(j = 0; j < M; j++)
+		scanf("%d", &S2[j]);
 
 	int median;
-	if((N1 + N2) % 2){
-		median = (N1 + N2) / 2;
-	}else{
-		median = (N1 + N2) / 2 - 1;
-	}
+	if((N + M) % 2)
+		median = (N + M) / 2;
+	else
+		median = (N + M) / 2 - 1;
 
 	i = j = 0;
-	while(i + j < median && i < N1 && j < N2){
-		if(num1[i] <= num2[j])
+	while(i + j < median && i < N && j < M)
+		if(S1[i] <= S2[j])
 			i++;
 		else
 			j++;
-	}
 
-	while(i + j < median){
-		if(j == N2)
+	while(i + j < median)
+		if(j == M)
 			i++;
 		else
 			j++;
-	}
 
-	if(j == N2 || (i != N1 && num1[i] <= num2[j]))
-		printf("%ld\n", num1[i]);
+	if(j == M || (i < N && S1[i] <= S2[j]))
+		printf("%d\n", S1[i]);
 	else
-		printf("%ld\n", num2[j]);
+		printf("%d\n", S2[j]);
 	return 0;
 }
