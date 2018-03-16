f8d9333a8f455a11fb45bc92e3acb86acfbf5de4
Wind <873890482@qq.com>
2018-03-16 09:53:22 +0800

    现在有点乱，以后整理

---
+++ PATAdvancedLevelPractise/1037.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <map>
+#include <string>
+using namespace std;
+int P, M, N;
+struct StudentNode
+{
+	string id;
+	int gp, gmid = -1, gfinal = -1, g = -1;
+}stuList[30001];
+
+bool cmp(StudentNode a, StudentNode b)
+{
+	if(a.g != b.g)
+		return a.g > b.g;
+	else
+	
+		return a.id < b.id;
+}
+
+int main(int argc, char const *argv[])
+{
+	int count = 0;
+	map<string, int>m;
+	scanf("%d %d %d", &P, &M, &N);
+	int i, score;
+	string name;
+	for(i = 0; i < P; i++)
+	{
+		cin >> name >> score;
+		if(score >= 200)
+		{
+			stuList[count].id = name;
+			stuList[count].gp = score;
+			stuList[count].gmid = stuList[count].gfinal = -1;
+			m[name] = count;
+			count++;
+		}
+	}
+	for(i = 0; i < M; i++)
+	{
+		cin >> name >> score;
+		if(m.find(name) != m.end())
+			stuList[m[name]].gmid = score;
+	}
+
+	for(i = 0; i < N; i++)
+	{
+		cin >> name >> score;
+		if(m.find(name) != m.end())
+			stuList[m[name]].gfinal = score;
+	}
+
+	for(i = 0; i < count; i++)
+	{
+		if(stuList[i].gmid <= stuList[i].gfinal)
+			stuList[i].g = stuList[i].gfinal;
+		else
+			stuList[i].g = (int)((double)stuList[i].gmid * 0.4 + (double)stuList[i].gfinal * 0.6 + 0.5);
+	}
+	sort(stuList, stuList + count, cmp);
+	for(i = 0; i < count; i++)
+		if(stuList[i].g >= 60)
+			printf("%s %d %d %d %d\n", stuList[i].id.c_str(), stuList[i].gp, stuList[i].gmid, stuList[i].gfinal, stuList[i].g);
+	return 0;
+}
--- PATAdvancedLevelPractise/1045.c
+++ PATAdvancedLevelPractise/1045.c
@@ -1,45 +1,35 @@
 #include <stdio.h>
-#include <string.h>
-
-int N, M, fa[201], L, gs[10001], c[201][10001];
-
-void Init()
-{
-	int i;
-	scanf("%d", &N);
-	scanf("%d", &M);
-	for(i = 1; i <= M; i++)
-		scanf("%d", &fa[i]);
-	scanf("%d", &L);
-	for(i = 1; i <= L; i++)
-		scanf("%d", &gs[i]);
-}
+int dp[205][10005];
+int fc[205], s[10005];
+int N, L;
 
 int max(int a, int b)
 {
-	return a >= b ? a : b;
+	return a > b ? a : b;
 }
-
 void LCS()
 {
-	int i, j;
-	for(i = 1; i <= M; i++)
-		c[i][0] = 0;
-	for(j = 0; j <= L; j++)
-		c[0][j] = 0;
-
-	for(i = 1; i <= M; i++)
-		for(j = 1; j <= L; j++)
-			if(fa[i] == gs[j])
-				c[i][j] = c[i][j-1] + 1;  // LCS 是 c[i-1][j-1] + 1,而此处可以重复
+	for(int i = 0; i <= N; i++)
+		dp[i][0] = 0;
+	for(int i = 1; i <= L; i++)
+		dp[0][i] = 0;
+	for(int i = 1; i <= N; i++)
+		for(int j = 1; j <= L; j++)
+			if(fc[i] == s[j])
+				dp[i][j] = dp[i][j - 1] + 1;
 			else
-				c[i][j] = max(c[i-1][j], c[i][j-1]);
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
 }
 
-int main(int argc, char const *argv[])
+int main(void)
 {
-	Init();
+	scanf("%d", &N);
+	for(int i = 0; i < N; i++)
+		scanf("%d", &fc[i + 1]);
+	scanf("%d", &L);
+	for(int i = 0; i < L; i++)
+		scanf("%d", &s[i] + 1);
 	LCS();
-	printf("%d\n", c[M][L]);
+	printf("%d", dp[N][L]);
 	return 0;
 }
