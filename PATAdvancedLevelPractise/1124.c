c0857b5a0d71f8d92ae8ae4b80c3196e2dd2f888
Wind <873890482@qq.com>
2018-03-16 21:00:21 +0800

    1124 AC

--- PATAdvancedLevelPractise/1124.c
+++ PATAdvancedLevelPractise/1124.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <string.h>
-
 int main(int argc, char const *argv[])
 {
 	int M, N, S;
 	scanf("%d %d %d", &M, &N, &S);
-	char Forwards[M][21];
-	int i;
-	for(i  = 0; i < M; i++)
+	char Forwards[M + 1][22];
+	for(int i  = 1; i <= M; i++)
 		scanf("%s", Forwards[i]);
-	int Stack[N], Top = -1;
-	S--;
-	while(S < M)
+	int Stack[M], Top = -1;
+	while(S <= M)
 	{
-		int j, flag = 1;
-		for(j = 0; j <= Top; j++)
+		int flag = 1;
+		for(int j = 0; j <= Top; j++)
 			if(strcmp(Forwards[Stack[j]], Forwards[S]) == 0)
 			{
 				flag = 0;
@@ -29,7 +26,7 @@ int main(int argc, char const *argv[])
 			S++;
 	}
 	if(Top >= 0)
-		for(i = 0; i <= Top; i++)
+		for(int i = 0; i <= Top; i++)
 			printf("%s\n", Forwards[Stack[i]]);
 	else
 		printf("Keep going...\n");
